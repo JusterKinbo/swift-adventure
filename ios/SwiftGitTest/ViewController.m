9489c6456ef48a096c3fb35e8ed9d17b0764c96b
贾春辉 <bjjiachunhui@corp.netease.com>
2017-12-18 17:52:25 +0800

    MOD:change project

---
+++ ios/SwiftGitTest/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  SwiftGitTest
+//
+//  Created by bjjiachunhui on 2017/12/18.
+//  Copyright © 2017年 bjjiachunhui. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor=UIColor.greenColor;
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
